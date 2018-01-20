1c2f7b5c37030f31f78badc1a4a68703110be2a5
Donald Trump <mail@simplekvm.net>
2018-01-20 21:50:04 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -43,13 +43,13 @@ systemctl enable vncserver@1.service
 su - user -c "vncserver -kill :1"
 systemctl start vncserver@1
 add-apt-repository ppa:webupd8team/java -y
-apt-get update
 # temp fix to webupd8 issue
 cd /var/lib/dpkg/info
 sed -i 's|JAVA_VERSION=8u151|JAVA_VERSION=8u161|' oracle-java8-installer.*
 sed -i 's|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/|' oracle-java8-installer.*
 sed -i 's|SHA256SUM_TGZ="c78200ce409367b296ec39be4427f020e2c585470c4eed01021feada576f027f"|SHA256SUM_TGZ="6dbc56a0e3310b69e91bb64db63a485bd7b6a8083f08e48047276380a0e2021e"|' oracle-java8-installer.*
 sed -i 's|J_DIR=jdk1.8.0_151|J_DIR=jdk1.8.0_161|' oracle-java8-installer.*
+apt-get update && apt get upgrade -y
 # end of fix.
 apt-get install oracle-java8-installer -y
 apt purge --autoremove xscreensaver -y
